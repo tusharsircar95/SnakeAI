b17a176da371d0bad49f056fd72af578bf9f2334
Tushar Sircar <tusharsircar@Tushars-MacBook-Pro.local>
2019-06-01 10:49:25 +0530

    Major refactoring of script

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 import pygame
 import numpy as np
+from pyeasyga import pyeasyga
+import random
 
-class GameObjects:
-
-    @staticmethod
-    def init(SCREEN_DIMS=(40,40),GRID_SQ=20,CONTROLLER=None):
-        GameObjects.SCREEN_DIMS = SCREEN_DIMS
-        GameObjects.GRID_SQ = GRID_SQ
-        GameObjects.GRID = np.zeros(SCREEN_DIMS)
-        GameObjects.SCORE = 0
-        GameObjects.STATE = "NOT_STARTED"
-        GameObjects.SNAKE = Snake ()
-        GameObjects.FOOD = Food ()
-        GameObjects.CONTROLLER = CONTROLLER
-        GameObjects.FONT = pygame.font.SysFont ( "comicsansms" , 30 )
 
 class Snake:
-    def __init__(self):
-        origin = int(GameObjects.SCREEN_DIMS[0]/2)
+    def __init__(self,grid):
+        origin = int(grid.shape[0]/2)
         self.direction = 'R'
         self.blobs = []
         for i in range(3):
             self.blobs.append((origin-i,origin))
-            GameObjects.GRID[origin-i][origin] = 1
         self.trail = None
+
+    def updateGrid(self, grid):
+        for blob in self.blobs:
+            grid[blob[0],blob[1]] = 1
+        return grid
 
     def followUp( self ):
         self.trail = self.blobs[-1]
@@ -33,18 +26,18 @@ class Snake:
     def grow( self ):
         self.blobs.append(self.trail)
 
-    def didCollide( self, headBlob ):
-        if headBlob[0] < 0 or headBlob[0] >= GameObjects.SCREEN_DIMS[0]:
+    def didCollide( self, headBlob, grid ):
+        if headBlob[0] < 0 or headBlob[0] >= grid.shape[0]:
             print('Side wall collission')
             return True
-        if headBlob[1] < 0 or headBlob[1] >= GameObjects.SCREEN_DIMS[1]:
+        if headBlob[1] < 0 or headBlob[1] >= grid.shape[1]:
             print ( 'TopBottom wall collission' )
             return True
-        if GameObjects.GRID[ headBlob[ 0 ] ][ headBlob[ 1 ] ] == 1:
+        if grid[ headBlob[ 0 ] ][ headBlob[ 1 ] ] == 1:
             print ( 'Self collission' )
-        return GameObjects.GRID[headBlob[0]][headBlob[1]] == 1
+        return grid[ headBlob[ 0 ] ][ headBlob[ 1 ] ] == 1
 
-    def move( self, food ):
+    def move( self, food, grid, score ):
         headBlob = self.blobs[ 0 ]
         if self.direction == 'U':
             headBlob = (headBlob[ 0 ] , headBlob[ 1 ] - 1)
@@ -55,25 +48,24 @@ class Snake:
         elif self.direction == 'R':
             headBlob = (headBlob[ 0 ] + 1 , headBlob[ 1 ])
 
-        if not self.didCollide(headBlob):
+        if not self.didCollide(headBlob, grid):
             self.followUp ()
             self.blobs[ 0 ] = headBlob
 
-            GameObjects.GRID[ self.trail[ 0 ] ][ self.trail[ 1 ] ] = 0
-            GameObjects.GRID[ headBlob[0] ][ headBlob[1] ] = 1
+            grid[ self.trail[ 0 ] ][ self.trail[ 1 ] ] = 0
+            grid[ headBlob[0] ][ headBlob[1] ] = 1
             if headBlob[0] == food.x and headBlob[1] == food.y:
                 self.grow()
-                GameObjects.SCORE = GameObjects.SCORE + 1
-                GameObjects.GRID[ self.trail[ 0 ] ][ self.trail[ 1 ] ] = 1
-                food.relocate()
-            return food,"ALIVE"
+                score = score + 1
+                grid[ self.trail[ 0 ] ][ self.trail[ 1 ] ] = 1
+                food.relocate(grid)
+            return food,grid,"ALIVE",score
         else:
             print('Collission:',headBlob,self.blobs[0])
-            return None,"DEAD"
+            return None,grid,"DEAD", score
 
     def setDirection( self, direction ):
         self.direction = direction
-        print(self.direction)
     def getDirection( self ):
         return self.direction
 
@@ -83,47 +75,149 @@ class Snake:
         elif newDirection == 'L' and self.getDirection() != 'R': self.setDirection('L')
         elif newDirection == 'R' and self.getDirection() != 'L': self.setDirection('R')
 
+
 class Food:
     def __init__(self):
         self.x = 2 * 1
         self.y = 2 * 1
 
-    def relocate( self ):
-        while GameObjects.GRID[self.x][self.y] != 0:
-            self.x = int(np.random.random() * GameObjects.SCREEN_DIMS[0])
-            self.y = int(np.random.random() * GameObjects.SCREEN_DIMS[1])
+    def relocate( self, grid ):
+        while grid[self.x][self.y] != 0:
+            self.x = int(np.random.random() * grid.shape[0])
+            self.y = int(np.random.random() * grid.shape[1])
 
 
 class Renderer:
-    def __init__(self):
-        pass
-    @staticmethod
-    def render_snake ( screen , snake ):
+    def __init__(self,SCREEN_DIMS,GRID_SQ,FONT):
+        self.SCREEN_DIMS = SCREEN_DIMS
+        self.GRID_SQ = GRID_SQ
+        self.FONT = FONT
+
+    def render_snake ( self, screen , snake ):
         for index,blob in enumerate(snake.blobs):
             pygame.draw.rect ( screen ,
                                (np.random.random () * 255 , np.random.random () * 255 , np.random.random () * 255) ,
-                               pygame.Rect ( blob[ 0 ] * GameObjects.GRID_SQ , blob[ 1 ] * GameObjects.GRID_SQ ,
-                                             GameObjects.GRID_SQ , GameObjects.GRID_SQ ) )
+                               pygame.Rect ( blob[ 0 ] * self.GRID_SQ , blob[ 1 ] * self.GRID_SQ ,
+                                             self.GRID_SQ , self.GRID_SQ ) )
 
-    @staticmethod
-    def render_food ( screen , food ):
+    def render_food ( self, screen , food ):
         if food is None:
             return
         pygame.draw.rect ( screen , (255 , 255 , 255) ,
-                           pygame.Rect ( food.x * GameObjects.GRID_SQ , food.y * GameObjects.GRID_SQ ,
-                                         GameObjects.GRID_SQ , GameObjects.GRID_SQ ) )
+                           pygame.Rect ( food.x * self.GRID_SQ , food.y * self.GRID_SQ ,
+                                         self.GRID_SQ , self.GRID_SQ ) )
 
-    @staticmethod
-    def render_game_over( screen, text ):
+    def render_game_over( self, screen, text ):
         screen.blit ( text ,
-                      ( (GameObjects.SCREEN_DIMS[ 0 ] * GameObjects.GRID_SQ / 2 - text.get_width () // 2) ,
-                        (GameObjects.SCREEN_DIMS[ 1 ] * GameObjects.GRID_SQ/ 2 - text.get_height ()) ))
-    @staticmethod
-    def render_score(screen, score):
-        text_score = GameObjects.FONT.render ( str(score) , True , (0 , 255 , 0) )
+                      ( (self.SCREEN_DIMS[ 0 ] * self.GRID_SQ / 2 - text.get_width () // 2) ,
+                        (self.SCREEN_DIMS[ 1 ] * self.GRID_SQ/ 2 - text.get_height ()) ))
+    def render_score(self, screen, score):
+        text_score = self.FONT.render ( str(score) , True , (0 , 255 , 0) )
         screen.blit ( text_score ,
-                      ((0.95*GameObjects.SCREEN_DIMS[ 0 ] * GameObjects.GRID_SQ - text_score.get_width () // 2) ,
-                       (2 * GameObjects.GRID_SQ - text_score.get_height ())) )
+                      ((0.95*self.SCREEN_DIMS[ 0 ] * self.GRID_SQ - text_score.get_width () // 2) ,
+                       (2 * self.GRID_SQ - text_score.get_height ())) )
+
+
+class Game:
+
+    def __init__(self,SCREEN_DIMS=(40,40),GRID_SQ=20,CONTROLLER=None):
+        pygame.init()
+        self.SCREEN_DIMS = SCREEN_DIMS
+        self.GRID_SQ = GRID_SQ
+        self.GRID = np.zeros(SCREEN_DIMS)
+        self.SCORE = 0
+        self.STATE = "NOT_STARTED"
+        self.SNAKE = Snake(self.GRID)
+        self.FOOD = Food()
+        self.CONTROLLER = CONTROLLER
+        self.FONT = pygame.font.SysFont("comicsansms", 30)
+        self.GRID = self.SNAKE.updateGrid(self.GRID)
+
+    def reset_game(self):
+        self.GRID = np.zeros(self.SCREEN_DIMS)
+        self.SCORE = 0
+        self.STATE = "NOT_STARTED"
+        self.SNAKE = Snake(self.GRID)
+        self.FOOD = Food()
+        self.GRID = self.SNAKE.updateGrid(self.GRID)
+
+    def set_controller(self,controller):
+        self.CONTROLLER = controller
+
+    def play(self):
+        self.reset_game()
+        renderer = Renderer(SCREEN_DIMS=self.SCREEN_DIMS,
+                            GRID_SQ=self.GRID_SQ,
+                            FONT=self.FONT)
+
+        screen = pygame.display.set_mode((self.SCREEN_DIMS[0] * self.GRID_SQ,
+                                          self.SCREEN_DIMS[1] * self.GRID_SQ))
+        quitGame = False
+        text_gameover = renderer.FONT.render("Game Over", True, (0, 255, 0))
+        clock = pygame.time.Clock()
+
+        while not quitGame:
+
+            events = pygame.event.get()
+            # Handle general events
+            for event in events:
+                if event.type == pygame.QUIT:
+                    quitGame = True
+                if event.type == pygame.KEYDOWN:
+
+                    if event.key == pygame.K_r:
+                        self.reset_game()
+                        print('Restarting...')
+
+                    if event.key == pygame.K_p:
+                        if self.STATE != "DEAD":
+                            self.STATE = "ALIVE"
+
+            # Game controls
+            if self.STATE == 'ALIVE':
+                newDirection = self.SNAKE.getDirection()
+
+                if self.CONTROLLER is None:
+                    for event in events:
+                        if event.type == pygame.QUIT:
+                            quitGame = True
+                        if event.type == pygame.KEYDOWN:
+                            if event.key == pygame.K_UP:
+                                newDirection = 'U'
+                            elif event.key == pygame.K_DOWN:
+                                newDirection = 'D'
+                            elif event.key == pygame.K_LEFT:
+                                newDirection = 'L'
+                            elif event.key == pygame.K_RIGHT:
+                                newDirection = 'R'
+                    self.SNAKE.updateDirection(newDirection)
+                elif callable(self.CONTROLLER):
+                    newDirection = self.CONTROLLER(self.SNAKE, self.FOOD, self.GRID)
+                    self.SNAKE.updateDirection(newDirection)
+
+            # Graphics rendering
+            screen.fill((0, 0, 0))
+            renderer.render_snake(screen, self.SNAKE)
+            renderer.render_food(screen, self.FOOD)
+            renderer.render_score(screen, self.SCORE)
+
+            if self.STATE == "ALIVE":
+                self.FOOD, self.GRID, self.STATE, self.SCORE = self.SNAKE.move(
+                    self.FOOD, self.GRID, self.SCORE)
+            elif self.STATE == "DEAD":
+                renderer.render_game_over(screen, text_gameover)
+                # quitGame = True
+            # for i in range(self.SCREEN_DIMS[0]):
+            #     for j in range(self.SCREEN_DIMS[1]):
+            #         if self.GRID[i][j] == 1:
+            #             print(i,j)
+
+            pygame.display.flip()
+            clock.tick(180)
+
+        print('Score: %d' % self.SCORE)
+        # pygame.quit()
+        return self.SCORE
 
 
 def sample_controller(snake,food,grid):
@@ -131,86 +225,9 @@ def sample_controller(snake,food,grid):
     return options[np.random.randint(4)]
 
 
+game = Game(SCREEN_DIMS=(30,30))
+game.set_controller(sample_controller)
+game.play()
 
 
-def play():
-    pygame.init ()
-    GameObjects.init ( CONTROLLER=None )
-    screen = pygame.display.set_mode ( (GameObjects.SCREEN_DIMS[ 0 ] * GameObjects.GRID_SQ ,
-                                        GameObjects.SCREEN_DIMS[ 1 ] * GameObjects.GRID_SQ) )
-    quitGame = False
-    text_gameover = GameObjects.FONT.render ( "Game Over" , True , (0 , 255 , 0) )
-    text_score = GameObjects.FONT.render ( "0" , True , (0 , 255 , 0) )
-    clock = pygame.time.Clock ()
-
-
-    while not quitGame:
-
-        '''
-        features = f ( snake, GameObject.GRID, food )
-        action = g ( features )
-        doAction (action)
-        '''
-        print('New Frame')
-        events = pygame.event.get()
-        # Handle general events
-        for event in events:
-            if event.type == pygame.QUIT:
-                quitGame = True
-            if event.type == pygame.KEYDOWN:
-
-                if event.key == pygame.K_r:
-                        GameObjects.init()
-                        print('Restarting...')
-
-                if event.key == pygame.K_p:
-                    if GameObjects.STATE != "DEAD":
-                        GameObjects.STATE = "ALIVE"
-
-        # Game controls
-        if GameObjects.STATE == 'ALIVE':
-            newDirection = GameObjects.SNAKE.getDirection ()
-
-            if GameObjects.CONTROLLER is None:
-                for event in events:
-                    if event.type == pygame.QUIT:
-                        quitGame = True
-                    if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_UP:
-                            newDirection = 'U'
-                        elif event.key == pygame.K_DOWN:
-                            newDirection = 'D'
-                        elif event.key == pygame.K_LEFT:
-                            newDirection = 'L'
-                        elif event.key == pygame.K_RIGHT:
-                            newDirection = 'R'
-                GameObjects.SNAKE.updateDirection ( newDirection )
-            elif callable(GameObjects.CONTROLLER):
-                newDirection = GameObjects.CONTROLLER(GameObjects.SNAKE,GameObjects.FOOD,GameObjects.GRID)
-                GameObjects.SNAKE.updateDirection ( newDirection )
-
-
-        # Graphics rendering
-        screen.fill((0,0,0))
-        Renderer.render_snake ( screen , GameObjects.SNAKE )
-        Renderer.render_food(screen,GameObjects.FOOD)
-        Renderer.render_score ( screen , GameObjects.SCORE )
-
-        if GameObjects.STATE == "ALIVE":
-            GameObjects.FOOD,GameObjects.STATE = GameObjects.SNAKE.move(GameObjects.FOOD)
-        elif GameObjects.STATE == "DEAD":
-            print(np.sum(GameObjects.GRID))
-            Renderer.render_game_over(screen,text_gameover)
-            quitGame = True
-        # for i in range(GameObjects.SCREEN_DIMS[0]):
-        #     for j in range(GameObjects.SCREEN_DIMS[1]):
-        #         if GameObjects.GRID[i][j] == 1:
-        #             print(i,j)
-
-        pygame.display.flip ()
-        clock.tick ( 40 )
-
-    print('Score: %d'%GameObjects.SCORE)
-    #pygame.quit()
-    return GameObjects.SCORE
 
